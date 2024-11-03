a8abc4c91f1b97be3650599079e73bd4a9d829d9
Akash-Suavrna <akashsuvarna2003@gmail.com>
2024-11-03 22:33:06 +0530

    First Commit

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './Configs/mongodb.js';
+
+// App Config
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// Initialize DB Connection
+await connectDB();
+
+// Initialize Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API routes
+app.get('/', (req, res) => res.send("API working"));
+
+app.listen(PORT, () => console.log("Server Running on port " + PORT));
